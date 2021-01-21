d42df5c2d9e58574ef926854ea3b4058256e7b03
Emmanuel Bakare <bakareemmanuel98@outlook.com>
2021-01-21 22:23:34 +0100

    Initial commit

---
+++ mprotect.c
@@ -0,0 +1,31 @@
+#include <sys/mman.h>
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <unistd.h>	
+#include <stdlib.h>
+
+#define BUF_SIZE 1024
+
+int main() {
+	char* buf;
+	printf("%u\n", sizeof(buf));
+
+	int pagesize = sysconf(_SC_PAGE_SIZE);
+	printf("Page size is %u\n", pagesize);
+
+	int status = posix_memalign((void**)&buf, pagesize, 4 * pagesize);
+	if(status == 0){
+	  puts("Error on memalign\n");
+	}
+
+	if(mprotect(buf, BUF_SIZE, PROT_READ) == -1){
+	  puts("Error occurred on mprotect\n");
+	}
+
+	char text[10] = "Hello";
+	puts("Should segfault after this\n");
+	strcpy(buf, text);
+	
+	puts(buf);
+}
